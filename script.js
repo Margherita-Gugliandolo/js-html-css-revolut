98206de7611ac459554201ba08dcb877289feddb
Margherita <mangoguglia@gmail.com>
2020-07-08 10:59:25 +0200

    js-html-css-revolut

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+
+//Menu principale all'entrata ed uscita del mouse
+
  $('.parent-dropdown').mouseenter(function() {
    $(this).children('.dropdown').show();
 });
@@ -6,10 +9,42 @@ $(document).ready(function(){
    $(this).children('.dropdown').hide();
 });
 
+// Menu hamburger dropdown
    $('div.list-name').click(function() {
 
    $(this).next('.faq').slideToggle();
+
+   // freccia su e giù
+   var freccia = $('i').hasClass('.fa-chevron-down');
+console.log(freccia);
+
  });
 
+// Menu Hamburger
+
+var menuHidden = true;
+
+$('i.fas.fa-bars').click(
+  function(){
+    if(menuHidden == true){
+      $('.hamburger-menu').addClass('show');
+      menuHidden = false;
+    } else {
+      $('.hamburger-menu').removeClass('show');
+      menuHidden = true;
+    }
+  }
+);
+
+$('i.fas.fa-times').click(
+  function(){
+    var active = $('.hamburger-menu').hasClass('show');
+    if(active == true){
+      $('.hamburger-menu').removeClass('show');
+    } else {
+      $('.hamburger-menu').addClass('show');
+    }
+  }
+);
 
 })
